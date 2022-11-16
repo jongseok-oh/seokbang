20cb4d2a7ec7a3f19bb81cce9398b798f7dfd83b
jongking <hynb14@gmail.com>
2022-11-16 16:15:50 +0900

    jongseok-oh::add MainView

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,7 +1,90 @@
 <template>
     <div>
-      
+        <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">
+                            <h1 class="mb-3" >Where is My Home</h1>
+                            <div class="row">
+                                <div class="col">
+                                    <input class="form-control form-control-lg" type="text" placeholder="검색하기"/>
+                                </div>
+                                <div class="col-auto"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="py-5" id="features">
+            <div class="container px-5 my-5">
+            <div class="row gx-5">
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                <h2 class="fw-bolder mb-0">엥? 이게 집이라고?</h2>
+                <img
+                    class="img-fluid rounded-3 my-5"
+                    src="https://youimg1.tripcdn.com/target/100v1f000001h1crh2F42_C_750_420.jpg_.webp?proc=source%2Ftrip"
+                    alt=""
+                />
+                </div>
+                <div class="col-lg-8">
+                    <div class="row gx-5 row-cols-md-2">
+                        <div class="col mb-5 h-100">
+                        <h2 class="h5">지혜롭게 내집 마련하기</h2>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action">가용자금 확인 및 대출 계획</a>
+                            <a href="#" class="list-group-item list-group-item-action">집 종류 및 지역 선택</a>
+                            <a href="#" class="list-group-item list-group-item-action">정보 수집 & 시세파악</a>
+                            <a href="#" class="list-group-item list-group-item-action">부동산 방문 & 집구경</a>
+                            <a href="#" class="list-group-item list-group-item-action">계약 및 잔금 치르기</a>
+                            <a href="#" class="list-group-item list-group-item-action">소유권 이전등기</a>
+                            <a href="#" class="list-group-item list-group-item-action">인테리어 공사</a>
+                        </div>
+                        
+                        </div>
+                        <div class="col mb-5 h-100">
+                            <h2 class="h5">부동산 뉴스</h2>
+                            <div class="list-group">
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-light">서울 입주 2년차 아파트 전셋값 1억400만원 올라</a>
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-light">12:16 거래한파...매수자가 사라졌다</a>
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-light">재건축 대안 또는 수직증축 리모델링... 추진 속...</a>
+                                <a href="#" class="list-group-item list-group-item-action list-group-item-light">고가기준 9억, 서울 아파트 중간값 됐다</a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+      </section>
     </div>
 </template>
 
-<style></style>
+<style>
+    .masthead{
+        height: 80vh;
+        background-image: url("../assets/RaffinaJ_contents_1__01.webp");
+        background-size: cover;
+    }
+    h1{
+        margin-top: 80%;
+    }
+</style>
+
+<script>
+
+export default {
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
